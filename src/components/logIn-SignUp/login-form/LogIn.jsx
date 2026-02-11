6cdd41c45a46d01b89a63998a55a4cc09c9b16f8
Rudresh <bamaniarudresh2008@gmail.com>
2026-02-11 17:49:04 +0530

    Completed the login page data checking between signUp users and now working on actual CRUD operations on students data storing...

--- src/components/logIn-SignUp/login-form/LogIn.jsx
+++ src/components/logIn-SignUp/login-form/LogIn.jsx
@@ -1,7 +1,31 @@
 import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import InputPass from "./inputPass";
 
 function LogIn() {
+    const [passType, setPassType] = useState("password");
+    const [user, setUser] = useState({email: "", password: ""});
+    // storing user email pass to check wheather they are signed up or not!!
+    const handleChange = (e) => {
+        const {id , value} = e.target;
+        setUser((prev) => ({...prev, [id]: value}));
+    }
+
+    // checking wheather they are signedUp or not...?
+    const handleVarificationOfUser = () => {
+
+        const existingData = localStorage.getItem('signUpUsers');
+        JSON.parse(existingData).forEach((data) => {
+            if(data.email == user.email){
+                console.log(true);
+            }
+            else{
+                console.log(false);
+            }
+         })
+     }
+
   return (
     <>
       <div className="min-h-screen flex justify-center items-center bg-gray-100">
@@ -13,7 +37,7 @@ function LogIn() {
 
           <div className="flex flex-col items-center w-full">
             <div className="w-full">
-              <form className="space-y-4">
+              <form className="space-y-4" onSubmit={(e) => e.preventDefault()}>
                 <div>
                   <label
                     htmlFor="email"
@@ -26,28 +50,30 @@ function LogIn() {
                     id="email"
                     className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-400"
                     placeholder="example@gmail.com"
+                    onChange={handleChange}
                   />
                 </div>
-
-                <div>
-                  <label
-                    htmlFor="pass"
-                    className="text-sm font-medium text-gray-700 block mb-1"
-                  >
-                    Enter your Password
-                  </label>
-                  <input
-                    type="password"
-                    id="pass"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-400"
-                    placeholder="name123"
-                  />
-                </div>
+                
+                <InputPass 
+                lab="Password"
+              type={passType}
+              id="password"
+              htmlFor="password"
+              placeholder="name1234"
+              onUpdate={handleChange}
+              passType={passType}
+              setPassType={setPassType}
+              value={user.password}/>
               </form>
             </div>
 
             <div className="w-full mt-5">
-              <button className="w-full flex items-center justify-center gap-2 bg-cyan-400 hover:bg-cyan-500 text-white font-semibold py-2 rounded-md transition">
+              <button className="w-full flex items-center justify-center gap-2 bg-cyan-400 hover:bg-cyan-500 text-white font-semibold py-2 rounded-md transition"
+              onClick={() => {
+                console.log(user)
+                handleVarificationOfUser()
+              }}
+              >
                 <FaUser /> Login
               </button>
             </div>
